c7c36ce710eae09a63e127bc1322e5c42e98fc17
Matt Masurka <m.masurka@gmail.com>
2026-04-03 17:40:54 -0700

    Tour section

--- src/components/SeatedTourWidget.tsx
+++ src/components/SeatedTourWidget.tsx
@@ -15,16 +15,22 @@ export function SeatedTourWidget() {
     link.href = "https://widget.seated.com/app-v2.css";
     document.head.appendChild(link);
 
+    const widgetScript = document.createElement("script");
+    widgetScript.id = `seated-55fdf2c0-script-${SEATED_ARTIST_ID}`;
+    widgetScript.setAttribute("data-artist-id", SEATED_ARTIST_ID);
+    widgetScript.src = "https://widget.seated.com/widget.js";
+    container.appendChild(widgetScript);
+
     const widgetDiv = document.createElement("div");
-    widgetDiv.id = `seated-${SEATED_ARTIST_ID}`;
+    widgetDiv.id = "seated-55fdf2c0";
     widgetDiv.setAttribute("data-artist-id", SEATED_ARTIST_ID);
     widgetDiv.setAttribute("data-css-version", "2");
     widgetDiv.setAttribute("data-start-load", "true");
     container.appendChild(widgetDiv);
 
-    const script = document.createElement("script");
-    script.src = "https://widget.seated.com/app.js";
-    container.appendChild(script);
+    const appScript = document.createElement("script");
+    appScript.src = "https://widget.seated.com/app.js";
+    container.appendChild(appScript);
 
     return () => {
       link.remove();
